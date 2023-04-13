6a12a59b41c08c962c065049f3c2c9c957eb3bb0
Hamidreza Bahramian <hamid.bahramian@gmail.com>
2023-04-13 09:07:42 -0400

    .

--- pages/customers/add.js
+++ pages/customers/add.js
@@ -1,18 +1,33 @@
+
 import styles from "../../styles/customers.add.module.css";
+import { useForm } from 'react-hook-form';
+import data from '../../data/data'
 
 const Customer = () => {
+    const { register, handleSubmit, errors, reset } = useForm();
+
+    let save = (values) => {
+
+        console.log(values);
+    }
 
     return(
         <>
             <div className={styles.container}>
                 <h1 className={styles.form_title}>Add a new customer</h1>
-                <form action="#">
+                <form action="#" onSubmit={handleSubmit(save)}>
                     <div className={styles.main_user_info}>
                         <div className={styles.user_input_box}>
                             <label htmlFor="fullName">Full Name</label>
                             <input type="text"
                                    id="fullName"
                                    name="fullName"
+                                   ref={register({
+                                       required: {
+                                           value: true,
+                                           message: 'You must enter your name',
+                                       },
+                                   })}
                                    placeholder="Enter Full Name"/>
                         </div>
 
@@ -28,7 +43,7 @@ const Customer = () => {
                     </div>
 
                     <div className={styles.form_submit_btn}>
-                        <input type="submit" value="save"></input>
+                        <input type="submit" value="save" ></input>
                     </div>
                 </form>
             </div>
